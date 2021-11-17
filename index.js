9db5736a868bbcdf1ab47a10db6be7a0ddb02d88
onticinc <onticinc@gmail.com>
2021-11-16 17:21:02 -0700

    playing with faker

--- index.js
+++ index.js
@@ -6,27 +6,36 @@ myModule.numbers();
 
 myModule.sayName();
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.readFile('story.txt', 'utf8', function (err, data) {
-    if (err) {
-        console.log("There was a problem reading the file.");
-    } else {
-        console.log(data);
-    }
-});
+// fs.readFile('story.txt', 'utf8', function (err, data) {
+//     if (err) {
+//         console.log("There was a problem reading the file.");
+//     } else {
+//         console.log(data);
+//     }
+// });
 
 
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-    res.write('Hello, World!');
-    res.end();
-})
-    .listen(8000);
+// http.createServer((req, res) => {
+//     res.write('Hello, World!');
+//     res.end();
+// })
+//     .listen(8000);
 
-module.exports = {
-    helloWorld,
-    numbers,
-    sayName
+const faker = require('faker');
+
+
+
+let randomName = faker.name.findName();
+console.log(randomName);
+
+let randomAddress = {
+    streetAddress: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zipCode: faker.address.zipCode(),
 }
+
+console.log(randomAddress);
